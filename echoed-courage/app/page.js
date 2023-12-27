60e48a2ff14d4d35e467168c1a6a2ca62742c5f0
0x18a6 <huffcbytecode@gmail.com>
2023-12-27 04:35:59 +0100

    feat: page setup, including system prompt

--- echoed-courage/app/page.js
+++ echoed-courage/app/page.js
@@ -34,3 +34,82 @@ const generatePrompt = (template, systemPrompt, messages) => {
     ...chat,
   ]);
 };
+
+export default function HomePage() {
+
+  const MAX_TOKENS = 4096;
+  const bottomRef = useRef(null);
+  const [messages, setMessages] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState(null);
+  const [starting, setStarting] = useState(false);
+  const [shortStory, setShortStory] = useState(true);
+
+  //   Llama params
+  const [model, setModel] = useState(MODELS[2]); // default to 70B
+  const [systemPrompt, setSystemPrompt] = useState(
+    `You're talking to a traumatized war victim, the victim can only communicate with a few words, you should generate a ${
+      !shortStory && "very"
+    } short story based on the victim's input, use I pronoun, do not add any details that are not mentioned in the input.`
+  );
+  const [temp, setTemp] = useState(0.75);
+  const [topP, setTopP] = useState(0.9);
+  const [maxTokens, setMaxTokens] = useState(800);
+
+  const [metrics, dispatch] = useReducer(metricsReducer, {
+    startedAt: null,
+    firstMessageAt: null,
+    completedAt: null,
+  });
+
+  const { complete, completion, setInput, input } = useCompletion({
+    api: "/api",
+    body: {
+      model: model.id,
+      systemPrompt: systemPrompt,
+      temperature: parseFloat(temp),
+      topP: parseFloat(topP),
+      maxTokens: parseInt(maxTokens),
+    },
+
+    onError: (error) => {
+      setError(error);
+    },
+    onResponse: (response) => {
+      setStarting(false);
+      setError(null);
+      dispatch({ type: "FIRST_MESSAGE" });
+    },
+    onFinish: () => {
+      dispatch({ type: "COMPLETE" });
+    },
+  });
+
+  const setAndSubmitPrompt = (newPrompt) => {
+    handleSubmit(newPrompt);
+  };
+
+  const handleSettingsSubmit = async (event) => {
+    event.preventDefault();
+    setOpen(false);
+    setSystemPrompt(event.target.systemPrompt.value);
+  };
+
+  const handleSubmit = async (userMessage) => {
+    setStarting(true);
+    const SNIP = "<!-- snip -->";
+
+    const messageHistory = [...messages];
+    if (completion.length > 0) {
+      messageHistory.push({
+        text: completion,
+        isUser: false,
+      });
+    }
+    messageHistory.push({
+      text: userMessage,
+      isUser: true,
+    });
+
+  
+}
